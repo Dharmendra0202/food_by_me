0bdb7cf35afb4809b25ef34fdeb24dce2cd217e1
Dharmendra <145327275+Dharmendra0202@users.noreply.github.com>
2026-02-07 21:15:31 +0530

    removing the page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import "./App.css";
+
 export default function App() {
   return (
     <div className="app-root">
